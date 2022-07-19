9ca8ff8fe566f9df3127abb058031dffad22d91b
dell <dell@192.168.0.100>
2022-07-19 11:36:43 -0700

    [anand]: demo2 add

---
+++ src/sample1/demo2.java
@@ -0,0 +1,14 @@
+package sample1;
+
+public class demo2 
+{
+	public static void main(String[] args) {
+		System.out.println("hii  good morning");
+		System.out.println("hello good afternoon");
+		System.out.println("hi good evening");
+		
+	}
+
+
+
+}

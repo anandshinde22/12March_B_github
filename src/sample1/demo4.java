e322086f6f4399ccc88e466c87c99070f82b7d7a
dell <dell@192.168.0.100>
2022-07-20 03:45:53 -0700

    [anand]: demo 4 added

---
+++ src/sample1/demo4.java
@@ -0,0 +1,12 @@
+package sample1;
+
+public class demo4 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("My name is anand");
+
+		System.out.println("i am from pune");
+	}
+
+}
